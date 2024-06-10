1cfdbdfcae51a0cefb512847a6390b1c0a79bc1a
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 14:54:48 +0300

    make build

---
+++ index.400663e7.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,r=0;r<e.length;r++){var l=e[r].cells,o=l[1].cloneNode(!0);e[r].insertBefore(o,l[l.length-1])}
+//# sourceMappingURL=index.400663e7.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = table.rows;
+for(let i = 0; i < rows.length; i++){
+    const cells = rows[i].cells;
+    const positionCell = cells[1];
+    const clonedPositionCell = positionCell.cloneNode(true);
+    rows[i].insertBefore(clonedPositionCell, cells[cells.length - 1]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
